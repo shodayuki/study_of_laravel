610577320dd15a48db04228a31dad6bf7513b3b0
shodayuki <yshoda1227@gmail.com>
2024-05-12 20:48:18 +0900

    add: メモ追加機能を実装

--- simple_memo_laravel/app/Http/Controllers/MemoController.php
+++ simple_memo_laravel/app/Http/Controllers/MemoController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Memo;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
 class MemoController extends Controller
 {
   /**
@@ -12,5 +16,21 @@ class MemoController extends Controller
     public function index()
     {
       return view('memo');
+    }
+
+  /**
+   * メモの追加
+   *
+   * @return RedirectResponse
+   */
+  public function add(): RedirectResponse
+  {
+      Memo::create([
+        'user_id' => Auth::id(),
+        'title' => '新規メモ',
+        'content' => ''
+      ]);
+
+      return redirect()->route('memo.index');
     }
 }
