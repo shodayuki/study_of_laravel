69112ba1d54e02e331a13be839831a25558c9538
shodayuki <yshoda1227@gmail.com>
2024-05-19 21:44:36 +0900

    add: CRUD処理を実装

--- order_management_system/app/Http/Controllers/PenController.php
+++ order_management_system/app/Http/Controllers/PenController.php
@@ -2,9 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pen;
 use Illuminate\Http\Request;
 
 class PenController extends Controller
 {
-    //
+    /**
+     * 一覧表示
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(): \Illuminate\Http\JsonResponse
+    {
+      $pens = Pen::all();
+
+      return response()->json([
+        'data' => $pens
+      ],200);
+    }
+
+    /**
+     * 登録
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request): \Illuminate\Http\JsonResponse
+    {
+      $pen = new Pen();
+
+      $pen->name = $request->name;
+      $pen->price = $requset->price;
+      $pen->save();
+
+      return response()->json([
+        'data' => $pen
+      ],201);
+    }
+
+    /**
+     * 指定のデータのみ取得
+     *
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function edit($id): \Illuminate\Http\JsonResponse
+    {
+      $pen = Pen::find($id);
+
+      return response()->json([
+        'data' => $pen
+      ], 200);
+    }
+
+    /**
+     * 更新
+     *
+     * @param Request $request
+     * @param Pen $pen
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, Pen $pen): \Illuminate\Http\JsonResponse
+    {
+      $pen->fill($request->all());
+      $pen->save();
+
+      return response()->json([
+        'data' => $pen
+      ], 200);
+    }
+
+    /**
+     * 削除
+     *
+     * @param Pen $pen
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function delete(Pen $pen): \Illuminate\Http\JsonResponse
+    {
+      $pen->delete();
+
+      return response()->json([
+        'message' => '無事に削除しました'
+      ], 200);
+    }
 }
--- order_management_system/app/Models/Pen.php
+++ order_management_system/app/Models/Pen.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Pen extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+      'name',
+      'price'
+    ];
 }
