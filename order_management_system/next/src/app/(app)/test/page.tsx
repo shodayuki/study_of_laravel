2ab472c22940059a00027bc11328e957cb858b91
shodayuki <yshoda1227@gmail.com>
2024-05-19 18:53:33 +0900

    add: 動作確認

---
+++ order_management_system/next/src/app/(app)/test/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Pages = () => {
+    return (
+        <div className="relative overflow-x-auto p-5">
+            <h1>My first Next.js</h1>
+        </div>
+    );
+}
+
+export default Pages;
