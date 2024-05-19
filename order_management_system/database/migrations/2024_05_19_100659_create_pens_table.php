29573f1d7eff81b1600bd8ea133aa8f303b3f9e6
shodayuki <yshoda1227@gmail.com>
2024-05-19 21:23:14 +0900

    add: シーダーとマイグレーションを実装

--- order_management_system/database/migrations/2024_05_19_100659_create_pens_table.php
+++ order_management_system/database/migrations/2024_05_19_100659_create_pens_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('pens', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->bigInteger('price');
             $table->timestamps();
         });
     }
