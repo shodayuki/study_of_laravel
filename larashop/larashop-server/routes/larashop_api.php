5f2efe94b12d33a6ec13b3126309eec6a06587ce
shodayuki <yshoda1227@gmail.com>
2024-08-15 10:09:15 +0900

    ログインAPI実装

--- larashop/larashop-server/routes/larashop_api.php
+++ larashop/larashop-server/routes/larashop_api.php
@@ -20,8 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/signup', [AuthController::class, 'signup']);
 Route::post('/auth/signup/verify', [AuthController::class, 'signupVerify']);
-//Route::post('/auth/signin', [AuthController::class, 'signin']);
-//
+Route::post('/auth/signin', [AuthController::class, 'signin']);
+
 //Route::get('/products', [ProductController::class, 'index']);
 //Route::get('/products/{product}', [ProductController::class, 'show']);
 //
